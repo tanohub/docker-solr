fbfd7fe26b5986080274c72332d78481a7f1dc87
tanohub <scecconello@gmail.com>
2023-10-25 00:56:24 +0200

    updated scripts/check_solr.py

--- scripts/check_solr.py
+++ scripts/check_solr.py
@@ -14,11 +14,18 @@ cluster_status_json = json.loads(cluster_status_request.text)
 
 #print(json.dumps(cluster_status_json, indent = 4, sort_keys=True))
 
+replica_state_down_counter = 0
 for collection in cluster_status_json["cluster"]["collections"]:
   for shard in cluster_status_json["cluster"]["collections"][collection]["shards"]:
-   for replica in cluster_status_json["cluster"]["collections"][collection]["shards"][shard]["replicas"]:
-    replica_state = cluster_status_json["cluster"]["collections"][collection]["shards"][shard]["replicas"][replica]["state"]
-    print("coll: " + collection + " --- shard: " + shard + " --- replica: " + replica + " --- state: " + replica_state )
+    for replica in cluster_status_json["cluster"]["collections"][collection]["shards"][shard]["replicas"]:
+      replica_state = cluster_status_json["cluster"]["collections"][collection]["shards"][shard]["replicas"][replica]["state"]
+      node_name = cluster_status_json["cluster"]["collections"][collection]["shards"][shard]["replicas"][replica]["node_name"]
+      if replica_state == 'down' :
+        replica_state_down_counter += 1 
+        print("coll: " + collection + " --- shard: " + shard + " --- node: " + node_name + "--- replica: " + replica + " --- state: " + replica_state )
+
+print("Found " + str(replica_state_down_counter) + " replica in down status")
+
 #        "state": cluster_status_json["cluster"]["collections"][collection]["shards"][shard]["replicas"][replica]["state"],
 #        "type": cluster_status_json["cluster"]["collections"][collection]["shards"][shard]["replicas"][replica]["type"],
 #        "leader": cluster_status_json["cluster"]["collections"][collection]["shards"][shard]["replicas"][replica]["leader"]
